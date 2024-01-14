1beee7473d9c973860aadfd29177e2c473241d90
Henny2 <henrieke.baunack@web.de>
2024-01-14 11:17:18 -0800

    grouping and hiding accessibility data

--- AccessibilityPractice/ContentView.swift
+++ AccessibilityPractice/ContentView.swift
@@ -25,16 +25,29 @@ struct ContentView: View {
     
     @State private var selectedPicture = Int.random(in: 0...3)
     var body: some View {
-        Image(pictures[selectedPicture])
-            .resizable()
-            .scaledToFit()
-            .onTapGesture {
-                selectedPicture = Int.random(in: 0...3)
+//        VStack{
+//            // image decorative tells swiftui that it is not needed to be read out, but it will still be if the user actively selects it or it's a button for example
+//            Image(decorative: "ales-krivec-15949")
+//            // if you hide a view, it will be completely ignored and never read out
+//                .accessibilityHidden(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+//            Image(pictures[selectedPicture])
+//                .resizable()
+//                .scaledToFit()
+//                .onTapGesture {
+//                    selectedPicture = Int.random(in: 0...3)
+//                }
+//                .accessibilityLabel(labels[selectedPicture])
+//                .accessibilityAddTraits(.isButton) // make user aware that the picture acts like a button, to avoid this it is better to just use the Button View and add the image via a label
+//                .accessibilityRemoveTraits(.isImage) // does not help so can remove this
+            HStack{
+                Text("Your score is")
+                Text("1000")
             }
-            .accessibilityLabel(labels[selectedPicture])
-            .accessibilityAddTraits(.isButton) // make user aware that the picture acts like a button, to avoid this it is better to just use the Button View and add the image via a label
-            .accessibilityRemoveTraits(.isImage) // does not help so can remove this
-         
+            //.accessibilityElement(children: .ignore) // this will make them selectable as one thing, vs two different views, aka groups them together
+            
+            .accessibilityElement(children: .combine) //this reads them together but it has a short pause between them
+            //.accessibilityLabel("your score is 1000") // with this, no matter which view you select, they both tell you the whole sentence which is good
+//        }
     }
 }
 
