1b69603e8daf56c4f511b0ad4b6159b404c1ac71
Henny2 <henrieke.baunack@web.de>
2024-01-14 10:38:33 -0800

    Initial Commit

---
+++ AccessibilityPractice/AccessibilityPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityPracticeApp.swift
+//  AccessibilityPractice
+//
+//  Created by Henrieke Baunack on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
